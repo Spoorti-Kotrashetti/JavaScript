4715c7a00b37363dcd2e7290beeb7b4b5c124955
SPOORTI <kspoorti08@gmail.com>
2020-07-29 09:40:54 +0530

    first commit

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+let numInput = document.querySelector("#string > input");
+let resOutput = document.querySelector("#yesno > output");
+//console.log(numInput);
+
+numInput.addEventListener('input',function(){
+    const num = numInput.value;
+    const revenum = reversed(num);
+    function reversed(num){
+        num=num+"";
+        return num.split("").reverse().join("");
+    }
+    //console.log(num===revenum);
+    const final = finalF(num,revenum); 
+    function finalF(a,b){
+        return a===b;
+    }
+    document.getElementById("yesno").innerHTML = final;
+});
